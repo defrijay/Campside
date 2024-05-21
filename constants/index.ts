c68aa46dccd21ad648fc1c811de6a6a6502562a4
def rijay <defrijay@gmail.com>
2024-05-21 21:14:14 +0700

    tambah folder proyekan

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_hilink_work', label: 'About Campside' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Integrated Offline Map',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'No need to worry about getting lost in the middle of the journey. Our application is equipped with detailed and accurate offline maps, allowing you to still find your way even without an internet connection. Explore every corner of Tenjo Laut nature with confidence.',
+    },
+    {
+      title: 'Location and Activities Guide',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Get complete information about camping locations, hiking trails and interesting activities that you can do at Tenjo Laut Kuningan Camp. This guide includes recommendations for the best places to pitch a tent, beautiful photo spots and challenging trekking routes.",
+    },
+    {
+      title: 'Community and Sharing Experiences',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Connect with fellow nature lovers through our community features. Share your camping experiences, photos and tips. Get inspired by other users adventure stories and build a wide network of friends among outdoor enthusiasts.',
+    },
+    {
+      title: 'Many new locations every month',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About Campside',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Climbing Campside', 'Hiking Campside', 'Boat Campside'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'campside@gmail.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
